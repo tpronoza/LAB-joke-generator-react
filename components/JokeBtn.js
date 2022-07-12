4efd522e293d18c564b8fb09f2bce2cddc510461
Tatiana Pronoza <tpronoza1@gmail.com>
2022-07-11 21:40:47 -0500

    added initial JokeBtn setup

---
+++ components/JokeBtn.js
@@ -0,0 +1,11 @@
+export default function JokeBtn() {
+  const handleClick = () => {
+
+  };
+  return (
+    <>
+      <h1> Joke Generator</h1>
+      <button type="button" onClick={handleClick}>Get Joke</button>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
+import JokeBtn from '../components/JokeBtn';
+
 function Home() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home!</h1>
-    </div>
+    <>
+      <JokeBtn />
+    </>
   );
 }
 
